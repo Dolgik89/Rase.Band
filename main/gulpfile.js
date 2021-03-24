098518fbdf54c576beb517ab89126934984f412c
Dolgalyov Eugene <dolgalyov.evgeniy@gmail.com>
2021-03-24 17:52:50 +0200

    Completed project settings

---
+++ main/gulpfile.js
@@ -0,0 +1,66 @@
+var gulp            = require('gulp');
+var sass            = require('gulp-sass');
+var autoprefixer    = require('gulp-autoprefixer');
+var gcmq            = require('gulp-group-css-media-queries');
+var clear           = require('gulp-minify-css');
+var uglify          = require('gulp-uglify');
+var concat          = require('gulp-concat');
+var nunjucks = require('gulp-nunjucks');
+var $ = {
+    gutil: require('gulp-util'),
+    size: require('gulp-size')
+};
+
+var basePaths = {
+    src: 'build/',
+    dest: 'source/'
+};
+var paths = {
+    styles: {
+        scss: basePaths.src + 'sass/',
+        css: basePaths.dest + 'css/'
+    },
+    js: {
+        build: basePaths.src + 'js/',
+        source: basePaths.dest + 'js/'
+    },
+    templates: {
+        njk: basePaths.src + 'templates/',
+        html: basePaths.dest
+    }
+};
+
+gulp.task('sass', function(done){
+    gulp.src(paths.styles.scss + '/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gcmq())
+        .pipe(clear())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(paths.styles.css));
+    done();
+});
+
+gulp.task('bundleJS', function (done) {
+    gulp.src(paths.js.build + '/**/*.js')
+        .pipe(concat('bundle.min.js'))
+        .pipe(gulp.dest(paths.js.source))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.js.source));
+    done();
+});
+
+gulp.task('nunjucks', function(done){
+    gulp.src(paths.templates.njk + '/**/[^_]*.njk')
+        .pipe(nunjucks.compile())
+        .pipe(gulp.dest(paths.templates.html));
+    done();
+})
+
+gulp.task('watch', function() {
+    gulp.watch(paths.styles.scss + '**/*.scss', gulp.series('sass'));
+    gulp.watch(paths.js.build + '**/*.js', gulp.series('bundleJS'));
+    gulp.watch(paths.templates.njk + '**/*.njk', gulp.series('nunjucks'));
+});
