1e0af0100e8ab5ad778111a0d772cee65699d921
Dolgalyov Eugene <dolgalyov.evgeniy@gmail.com>
2021-03-25 18:23:04 +0200

    Completed header and footer (responsive)

--- main/build/js/script.js
+++ main/build/js/script.js
@@ -1,9 +1,12 @@
+
+//Search form
 const search_form = document.querySelector('.search-form form')
 const search_input = search_form.querySelector('input')
 
 search_form.addEventListener('submit', e => {
     let parent = e.target.closest('.search-form');
-    if ( !parent.classList.contains('search-form_focus') ) {
+
+    if ( parent.querySelector('input').value === '' ) {
         e.preventDefault()
         parent.classList.add('search-form_focus')
         parent.querySelector('input').focus()
@@ -14,4 +17,11 @@ search_input.addEventListener('blur', e => {
         let parent = e.target.closest('.search-form')
         parent.classList.remove('search-form_focus')
     }
+})
+
+//Menu
+const mobile_button = document.querySelector('.header__mobile-button')
+const trigger_click = 'click' || 'touchstart'
+mobile_button.addEventListener(trigger_click, function (e) {
+    e.target.closest('.header').classList.toggle('header_open')
 })
